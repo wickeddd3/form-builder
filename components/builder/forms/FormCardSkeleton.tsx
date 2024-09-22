430172aa6c5dd17d9468e36843c4672c71337393
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-23 04:16:55 +0800

    feat: add FormCardSkeleton

---
+++ components/builder/forms/FormCardSkeleton.tsx
@@ -0,0 +1,7 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+function FormCardSkeleton() {
+  return <Skeleton className="border-2 border-primary-/20 h-[190px] w-full" />;
+}
+
+export default FormCardSkeleton;
