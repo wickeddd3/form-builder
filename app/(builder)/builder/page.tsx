2f003ed25bc1c4a42fcf56e9187c3b09a90e57e1
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-23 01:04:21 +0800

    feat: add builder home page

--- app/(builder)/builder/page.tsx
+++ app/(builder)/builder/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-  return <div>Page</div>;
+import { getFormStats } from "@/actions/form";
+import CreateFormButton from "@/components/builder/forms/CreateFormButton";
+import StatsCards from "@/components/builder/stats/StatsCards";
+import { Separator } from "@/components/ui/separator";
+import { Suspense } from "react";
+
+export default async function Page() {
+  const stats = await getFormStats();
+
+  return (
+    <div className="w-full p-4">
+      <Suspense fallback={<StatsCards loading={true} />}>
+        <StatsCards loading={false} data={stats} />
+      </Suspense>
+      <Separator className="my-6" />
+      <h2 className="text-4xl font-bold col-span-2">My forms</h2>
+      <Separator className="my-6" />
+      <CreateFormButton />
+    </div>
+  );
 }
