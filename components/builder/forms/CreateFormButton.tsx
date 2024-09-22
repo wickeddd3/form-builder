9336c42fba016c607ec84c42839a00aca6ed2e64
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-23 04:21:12 +0800

    add: create new form button styles

--- components/builder/forms/CreateFormButton.tsx
+++ components/builder/forms/CreateFormButton.tsx
@@ -10,12 +10,21 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import CreateForm from "@/components/builder/forms/CreateForm";
+import { BsFileEarmarkPlus } from "react-icons/bs";
 
 function CreateFormButton() {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button>Create new form</Button>
+        <Button
+          variant={"outline"}
+          className="group border border-primary/20 h-[190px] items-center justify-center flex flex-col hover:border-primary hover:cursor-pointer border-dashed gap-4"
+        >
+          <BsFileEarmarkPlus className="h-8 w-8 text-muted-foreground group-hover:text-primary" />
+          <p className="font-bold text-xl text-muted-foreground group-hover:text-primary">
+            Create new form
+          </p>
+        </Button>
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
