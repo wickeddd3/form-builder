d1693436474004f7e5ccfb2353f3336b3fb93702
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-23 03:18:28 +0800

    feat: add CreateForm component

---
+++ components/builder/forms/CreateForm.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  createFormSchema,
+  CreateFormValues,
+} from "@/components/validation/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { ImSpinner2 } from "react-icons/im";
+import { toast } from "@/hooks/use-toast";
+import { createForm } from "@/actions/form";
+
+function CreateForm() {
+  const form = useForm<CreateFormValues>({
+    resolver: zodResolver(createFormSchema),
+    defaultValues: {
+      name: "",
+      description: "",
+    },
+  });
+
+  const {
+    handleSubmit,
+    control,
+    formState: { isSubmitting },
+  } = form;
+
+  async function onSubmit(values: CreateFormValues) {
+    try {
+      await createForm(values);
+      toast({
+        title: "Success",
+        description: "Form created successfully",
+      });
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Something went wrong, please try again later",
+        variant: "destructive",
+      });
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
+        <FormField
+          control={control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea rows={5} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+      <Button
+        onClick={handleSubmit(onSubmit)}
+        disabled={isSubmitting}
+        className="w-full mt-4"
+      >
+        {!isSubmitting && <span>Save</span>}
+        {isSubmitting && <ImSpinner2 className="animate-spin" />}
+      </Button>
+    </Form>
+  );
+}
+
+export default CreateForm;
