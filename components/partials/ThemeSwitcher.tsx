3721909d7655f7ecd3177eb21b3a75dbc9970798
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-22 16:46:35 +0800

    feat: add ThemeSwitcher component

---
+++ components/partials/ThemeSwitcher.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { LuMonitor, LuMoonStar, LuSunDim, LuSunMedium } from "react-icons/lu";
+
+function ThemeSwitcher() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <Tabs defaultValue={theme}>
+      <TabsList className="border">
+        <TabsTrigger value="light" onClick={() => setTheme("light")}>
+          {theme === "dark" ? (
+            <LuSunDim className="h-[1.2rem] w-[1.2rem]" />
+          ) : (
+            <LuSunMedium className="h-[1.2rem] w-[1.2rem]" />
+          )}
+        </TabsTrigger>
+        <TabsTrigger value="dark" onClick={() => setTheme("dark")}>
+          <LuMoonStar className="h-[1.2rem] w-[1.2rem] rotate-90 transition-all dark:rotate-0" />
+        </TabsTrigger>
+        <TabsTrigger value="system" onClick={() => setTheme("system")}>
+          <LuMonitor className="h-[1.2rem] w-[1.2rem]" />
+        </TabsTrigger>
+      </TabsList>
+    </Tabs>
+  );
+}
+
+export default ThemeSwitcher;
