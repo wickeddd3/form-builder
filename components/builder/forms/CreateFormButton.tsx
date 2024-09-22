d71340f0cd2ebdb0eefff8f2c6fd6d6f1e36fef9
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-23 01:05:15 +0800

    feat: add CreateFormButton component

---
+++ components/builder/forms/CreateFormButton.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+function CreateFormButton() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Create new form</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create form</DialogTitle>
+          <DialogDescription>
+            Create a new form to start collecting responses
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default CreateFormButton;
