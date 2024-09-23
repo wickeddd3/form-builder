e9999089d4a6527c355cab5a540adb8f1afbdc55
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-24 03:49:11 +0800

    feat: wrap Designer in DndContext

--- components/builder/form-builder/designer/Designer.tsx
+++ components/builder/form-builder/designer/Designer.tsx
@@ -1,14 +1,17 @@
 import DesignerEditor from "@/components/builder/form-builder/designer/DesignerEditor";
 import DesignerSidebar from "@/components/builder/form-builder/designer/DesignerSidebar";
+import { DndContext } from "@dnd-kit/core";
 
 function Designer() {
   return (
-    <div className="flex w-full flex-grow items-center justify-center relative overflow-y-auto h-[200px] bg-accent bg-[url(/background/paper.svg)] dark:bg-[url(/background/paper-dark.svg)]">
-      <div className="flex w-full h-full">
-        <DesignerEditor />
-        <DesignerSidebar />
+    <DndContext>
+      <div className="flex w-full flex-grow items-center justify-center relative overflow-y-auto h-[200px] bg-accent bg-[url(/background/paper.svg)] dark:bg-[url(/background/paper-dark.svg)]">
+        <div className="flex w-full h-full">
+          <DesignerEditor />
+          <DesignerSidebar />
+        </div>
       </div>
-    </div>
+    </DndContext>
   );
 }
 
