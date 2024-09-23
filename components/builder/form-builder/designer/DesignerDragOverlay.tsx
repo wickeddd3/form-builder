1f4b122ecde4926c52d405cf4b2524638c139a5b
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-24 04:08:40 +0800

    feat: listen to dragged event and set draggedItem, if isDesignerElementButton set DesignerElementButtonDragOverlay as node to show to DragOverlay

--- components/builder/form-builder/designer/DesignerDragOverlay.tsx
+++ components/builder/form-builder/designer/DesignerDragOverlay.tsx
@@ -1,5 +1,34 @@
+import { Active, DragOverlay, useDndMonitor } from "@dnd-kit/core";
+import { useState } from "react";
+import { DesignerElementButtonDragOverlay } from "@/components/builder/form-builder/designer/DesignerElementButton";
+
 function DesignerDragOverlay() {
-  return <div>DesignerDragOverlay</div>;
+  const [draggedItem, setDraggedItem] = useState<Active | null>(null);
+
+  useDndMonitor({
+    onDragStart: (event) => {
+      console.log(event);
+      setDraggedItem(event.active);
+    },
+    onDragCancel: () => {
+      setDraggedItem(null);
+    },
+    onDragEnd: () => {
+      setDraggedItem(null);
+    },
+  });
+
+  if (!draggedItem) return null;
+
+  let node = null;
+  const isSidebarBtnElement =
+    draggedItem.data?.current?.isDesignerElementButton;
+
+  if (isSidebarBtnElement) {
+    node = <DesignerElementButtonDragOverlay />;
+  }
+
+  return <DragOverlay>{node}</DragOverlay>;
 }
 
 export default DesignerDragOverlay;
