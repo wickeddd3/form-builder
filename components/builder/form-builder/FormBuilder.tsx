d3e08c3f5507101590d4dcb6977c295b85ed8ccb
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-24 02:29:56 +0800

    add: Designer to FormBuilder

--- components/builder/form-builder/FormBuilder.tsx
+++ components/builder/form-builder/FormBuilder.tsx
@@ -2,11 +2,13 @@
 
 import { Form } from "@prisma/client";
 import ActionButtons from "@/components/builder/form-builder/action-buttons/ActionButtons";
+import Designer from "@/components/builder/form-builder/designer/Designer";
 
 function FormBuilder({ form }: { form: Form }) {
   return (
     <main className="flex flex-col w-full">
       <ActionButtons form={form} />
+      <Designer />
     </main>
   );
 }
