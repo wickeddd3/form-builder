97877a24d6271195173edb9216393c1ba7786fab
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-24 02:55:08 +0800

    add: DesignerSidebar width and style

--- components/builder/form-builder/designer/DesignerSidebar.tsx
+++ components/builder/form-builder/designer/DesignerSidebar.tsx
@@ -1,5 +1,7 @@
 function DesignerSidebar() {
-  return <div>DesignerSidebar</div>;
+  return (
+    <aside className="w-[400px] max-w-[400px] flex flex-col flex-grow gap-2 border-l-2 border-muted p-4 bg-background overflow-y-auto h-full"></aside>
+  );
 }
 
 export default DesignerSidebar;
