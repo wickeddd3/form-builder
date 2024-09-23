a03247247b3cd4c9dff2e89c885e0862a4823853
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-24 03:10:49 +0800

    feat: add DesignerElements

---
+++ components/builder/form-builder/designer/DesignerElements.tsx
@@ -0,0 +1,12 @@
+import DesignerElementButton from "@/components/builder/form-builder/designer/DesignerElementButton";
+
+function DesignerElements() {
+  return (
+    <div className="flex flex-col gap-4">
+      <h6 className="text-md font-medium">Form Elements</h6>
+      <DesignerElementButton />
+    </div>
+  );
+}
+
+export default DesignerElements;
--- components/builder/form-builder/designer/DesignerSidebar.tsx
+++ components/builder/form-builder/designer/DesignerSidebar.tsx
@@ -1,6 +1,10 @@
+import DesignerElements from "@/components/builder/form-builder/designer/DesignerElements";
+
 function DesignerSidebar() {
   return (
-    <aside className="w-[400px] max-w-[400px] flex flex-col flex-grow gap-2 border-l-2 border-muted p-4 bg-background overflow-y-auto h-full"></aside>
+    <aside className="w-[400px] max-w-[400px] flex flex-col flex-grow gap-2 border-l-2 border-muted p-4 bg-background overflow-y-auto h-full">
+      <DesignerElements />
+    </aside>
   );
 }
 
