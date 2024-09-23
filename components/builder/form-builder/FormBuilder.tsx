a93a2f239d6505a6bf4bf98e09781094c8547b04
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-24 00:11:09 +0800

    feat: add ActionButtons in builder

--- components/builder/form-builder/FormBuilder.tsx
+++ components/builder/form-builder/FormBuilder.tsx
@@ -1,9 +1,14 @@
 "use client";
 
 import { Form } from "@prisma/client";
+import ActionButtons from "@/components/builder/form-builder/action-buttons/ActionButtons";
 
 function FormBuilder({ form }: { form: Form }) {
-  return <div>{form.name}</div>;
+  return (
+    <main className="flex flex-col w-full">
+      <ActionButtons form={form} />
+    </main>
+  );
 }
 
 export default FormBuilder;
