e08a61b0f3f4f2f1e47b1e05676e7f70d20885ed
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-23 23:47:02 +0800

    feat: fetch form using getFormById action and pass form data to FormBuilder

--- app/(builder)/builder/[id]/page.tsx
+++ app/(builder)/builder/[id]/page.tsx
@@ -1,7 +1,15 @@
+import { getFormById } from "@/actions/form";
 import FormBuilder from "@/components/builder/FormBuilder";
 
-function Page() {
-  return <FormBuilder />;
+async function Page({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const form = await getFormById(id);
+
+  if (!form) {
+    throw new Error("Form not found");
+  }
+
+  return <FormBuilder form={form} />;
 }
 
 export default Page;
