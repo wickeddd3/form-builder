096e470a61c3fbe2def9c428be158bac040b4587
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-23 23:47:24 +0800

    feat: add form props

--- components/builder/FormBuilder.tsx
+++ components/builder/FormBuilder.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-function FormBuilder() {
-  return <div>FormBuilder</div>;
+import { Form } from "@prisma/client";
+
+function FormBuilder({ form }: { form: Form }) {
+  return <div>{form.name}</div>;
 }
 
 export default FormBuilder;
