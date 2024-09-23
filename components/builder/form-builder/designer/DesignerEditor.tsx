92769b3abcfeb24c1ccc1841b1e125e302a5ed4f
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-24 02:54:43 +0800

    feat: set DesignerEditor as droppable

--- components/builder/form-builder/designer/DesignerEditor.tsx
+++ components/builder/form-builder/designer/DesignerEditor.tsx
@@ -1,5 +1,21 @@
+import { useDroppable } from "@dnd-kit/core";
+
 function DesignerEditor() {
-  return <div>DesignerEditor</div>;
+  const droppable = useDroppable({
+    id: "designer-editor-drop-area",
+    data: {
+      isDesignerDropArea: true,
+    },
+  });
+
+  return (
+    <div className="p-4 w-full">
+      <div
+        ref={droppable.setNodeRef}
+        className="bg-background max-w-[920px] h-full m-auto rounded-xl flex flex-col flex-grow items-center justify-start flex-1"
+      ></div>
+    </div>
+  );
 }
 
 export default DesignerEditor;
