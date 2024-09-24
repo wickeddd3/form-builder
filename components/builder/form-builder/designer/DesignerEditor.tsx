f89523e031d2834e929a4b648ae751f102bd2bf7
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-24 23:08:13 +0800

    feat: add dragged element to designer elements array and show elements array in designer editor

--- components/builder/form-builder/designer/DesignerEditor.tsx
+++ components/builder/form-builder/designer/DesignerEditor.tsx
@@ -1,5 +1,11 @@
 import { DragEndEvent, useDndMonitor, useDroppable } from "@dnd-kit/core";
 import { RiDragDropLine } from "react-icons/ri";
+import {
+  ElementsType,
+  FormElements,
+} from "@/components/builder/form-builder/FormElements";
+import useDesigner from "@/hooks/use-designer";
+import { idGenerator } from "@/lib/idGenerator";
 
 function DesignerEditor() {
   const droppable = useDroppable({
@@ -9,9 +15,29 @@ function DesignerEditor() {
     },
   });
 
+  const { elements, addElement } = useDesigner();
+
   useDndMonitor({
     onDragEnd: (event: DragEndEvent) => {
-      console.log(event);
+      const { active, over } = event;
+      if (!active || !over) return;
+
+      const isDesignerElementButton =
+        active.data?.current?.isDesignerElementButton;
+      const isDroppingOverDesignerDropArea =
+        over.data?.current?.isDesignerDropArea;
+
+      const droppingDesignerElementButtonOverDesignerDropArea =
+        isDesignerElementButton && isDroppingOverDesignerDropArea;
+
+      if (droppingDesignerElementButtonOverDesignerDropArea) {
+        const type = active.data?.current?.type;
+        const newElement = FormElements[type as ElementsType].construct(
+          idGenerator()
+        );
+        addElement(elements.length, newElement);
+        return;
+      }
     },
   });
 
@@ -27,6 +53,17 @@ function DesignerEditor() {
             <p className="text-xl md:text-3xl text-muted-foreground items-center font-bold">
               Drag and Drop here
             </p>
+          </div>
+        )}
+        {elements.length > 0 && (
+          <div className="flex flex-col w-full gap-2 p-4">
+            {elements.map((element) => {
+              const DesignerElement =
+                FormElements[element.type].designerComponent;
+              return (
+                <DesignerElement key={element.id} elementInstance={element} />
+              );
+            })}
           </div>
         )}
       </div>
