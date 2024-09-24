08f6cae200e0d8b1a37c2ffc045fecd7906b9fac
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-25 02:39:10 +0800

    feat: add DndContext sensors

--- components/builder/form-builder/designer/Designer.tsx
+++ components/builder/form-builder/designer/Designer.tsx
@@ -1,11 +1,32 @@
-import { DndContext } from "@dnd-kit/core";
+import {
+  DndContext,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
 import DesignerEditor from "@/components/builder/form-builder/designer/editor/DesignerEditor";
 import DesignerSidebar from "@/components/builder/form-builder/designer/sidebar/DesignerSidebar";
 import DesignerDragOverlay from "@/components/builder/form-builder/designer/DesignerDragOverlay";
 
 function Designer() {
+  const mouseSensor = useSensor(MouseSensor, {
+    activationConstraint: {
+      distance: 10, //10px
+    },
+  });
+
+  const touchSensor = useSensor(TouchSensor, {
+    activationConstraint: {
+      delay: 300,
+      tolerance: 5,
+    },
+  });
+
+  const sensors = useSensors(mouseSensor, touchSensor);
+
   return (
-    <DndContext>
+    <DndContext sensors={sensors}>
       <div className="flex w-full flex-grow items-center justify-center relative overflow-y-auto h-[200px] bg-accent bg-[url(/background/paper.svg)] dark:bg-[url(/background/paper-dark.svg)]">
         <div className="flex w-full h-full">
           <DesignerEditor />
