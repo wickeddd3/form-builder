02f3f82f322c8dc6cdd6c80e8cb5447e3b9a6507
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-25 02:18:44 +0800

    feat: set overlay element to show when designer element is being dragged

--- components/builder/form-builder/designer/DesignerDragOverlay.tsx
+++ components/builder/form-builder/designer/DesignerDragOverlay.tsx
@@ -5,8 +5,10 @@ import {
   ElementsType,
   FormElements,
 } from "@/components/builder/form-builder/FormElements";
+import useDesigner from "@/hooks/use-designer";
 
 function DesignerDragOverlay() {
+  const { elements } = useDesigner();
   const [draggedItem, setDraggedItem] = useState<Active | null>(null);
 
   useDndMonitor({
@@ -23,15 +25,33 @@ function DesignerDragOverlay() {
 
   if (!draggedItem) return null;
 
-  let node = null;
+  let node = <div>No drag overlay</div>;
+
+  // Overlay element to show when DesignerElementButton is dragged
   const isDesignerElementButton =
     draggedItem.data?.current?.isDesignerElementButton;
-
   if (isDesignerElementButton) {
     const type = draggedItem.data?.current?.type as ElementsType;
     node = (
       <DesignerElementButtonDragOverlay formElement={FormElements[type]} />
     );
+  }
+
+  // Overlay element to show when DesignerElement is dragged
+  const isDesignerElement = draggedItem.data?.current?.isDesignerElement;
+  if (isDesignerElement) {
+    const elementId = draggedItem.data?.current?.elementId;
+    const element = elements.find((el) => el.id === elementId);
+    if (!element) {
+      node = <div>Element not found</div>;
+    } else {
+      const DesignerComponent = FormElements[element.type].designerComponent;
+      node = (
+        <div className="flex bg-accent border rounded-md h-[120px] w-full py-2 px-4 opacity-80 pointer pointer-events-none">
+          <DesignerComponent elementInstance={element} />
+        </div>
+      );
+    }
   }
 
   return <DragOverlay>{node}</DragOverlay>;
