cd4f27f59dc9fe6990b36eaea686ae5ccf705adc
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-25 02:36:12 +0800

    feat: add removable of DesignerElement from DesignerEditor

--- components/builder/form-builder/designer/editor/DesignerEditorElementWrapper.tsx
+++ components/builder/form-builder/designer/editor/DesignerEditorElementWrapper.tsx
@@ -1,12 +1,20 @@
 import { useDraggable, useDroppable } from "@dnd-kit/core";
 import { FormElementInstance } from "@/components/builder/form-builder/FormElements";
 import DesignerEditorElement from "@/components/builder/form-builder/designer/editor/DesignerEditorElement";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import useDesigner from "@/hooks/use-designer";
+import { MdDeleteOutline } from "react-icons/md";
 
 function DesignerEditorElementWrapper({
   element,
 }: {
   element: FormElementInstance;
 }) {
+  const { removeElement } = useDesigner();
+  const [mouseIsOver, setMouseIsOver] = useState<boolean>(false);
+
   const topHalf = useDroppable({
     id: element.id + "-top",
     data: {
@@ -42,23 +50,58 @@ function DesignerEditorElementWrapper({
       {...draggable.listeners}
       {...draggable.attributes}
       className="relative h-[120px] flex flex-col text-foreground hover:cursor-pointer rounded-md ring-1 ring-accent ring-inset"
+      onMouseEnter={() => {
+        setMouseIsOver(true);
+      }}
+      onMouseLeave={() => {
+        setMouseIsOver(false);
+      }}
+      onClick={(e) => {
+        e.stopPropagation();
+      }}
     >
       <div
         ref={topHalf.setNodeRef}
         className="absolute w-full h-1/2 rounded-t-md"
       ></div>
-      {topHalf.isOver && (
-        <div className="absolute top-0 w-full rounded-md h-[7px] bg-primary rounded-b-none"></div>
-      )}
-      <div className="flex w-full h-[120px] items-center rounded-md bg-accent/40 px-4 py-2 pointer-events-none opacity-100">
-        <DesignerEditorElement element={element} />
-      </div>
       <div
         ref={bottomHalf.setNodeRef}
         className="absolute w-full bottom-0 h-1/2 rounded-b-md"
       ></div>
+      {topHalf.isOver && (
+        <div className="absolute top-0 w-full rounded-md h-[7px] bg-primary rounded-b-none"></div>
+      )}
       {bottomHalf.isOver && (
         <div className="absolute bottom-0 w-full rounded-md h-[7px] bg-primary rounded-t-none"></div>
+      )}
+      <div
+        className={cn(
+          "flex w-full h-[120px] items-center rounded-md bg-accent/40 px-4 py-2 pointer-events-none opacity-100",
+          mouseIsOver && "opacity-30"
+        )}
+      >
+        <DesignerEditorElement element={element} />
+      </div>
+      {mouseIsOver && (
+        <>
+          <div className="absolute right-0 h-full">
+            <Button
+              className="flex justify-center h-full border rounded-md rounded-l-none bg-red-500"
+              variant={"outline"}
+              onClick={(e) => {
+                e.stopPropagation();
+                removeElement(element.id);
+              }}
+            >
+              <MdDeleteOutline className="h-6 w-6" />
+            </Button>
+          </div>
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-pulse">
+            <p className="text-muted-foreground text-sm">
+              Click for properties or drag to move
+            </p>
+          </div>
+        </>
       )}
     </div>
   );
