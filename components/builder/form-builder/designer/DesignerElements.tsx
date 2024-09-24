40a093daae7c479ec6764f2e2ee6c630d2918f27
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-24 23:05:31 +0800

    feat: pass formElement prop in DesignerElementButton

--- components/builder/form-builder/designer/DesignerElements.tsx
+++ components/builder/form-builder/designer/DesignerElements.tsx
@@ -1,10 +1,11 @@
 import DesignerElementButton from "@/components/builder/form-builder/designer/DesignerElementButton";
+import { FormElements } from "@/components/builder/form-builder/FormElements";
 
 function DesignerElements() {
   return (
     <div className="flex flex-col gap-4">
       <h6 className="text-md font-medium">Form Elements</h6>
-      <DesignerElementButton />
+      <DesignerElementButton formElement={FormElements.TextField} />
     </div>
   );
 }
