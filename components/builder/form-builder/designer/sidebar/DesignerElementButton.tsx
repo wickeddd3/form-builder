b4628bbe34a49df2aaeea1e52a172a94f796d19b
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-25 02:45:20 +0800

    feat: highlight DesignerElementButton when being dragged

--- components/builder/form-builder/designer/sidebar/DesignerElementButton.tsx
+++ components/builder/form-builder/designer/sidebar/DesignerElementButton.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { useDraggable } from "@dnd-kit/core";
 import { FormElement } from "@/components/builder/form-builder/FormElements";
+import { cn } from "@/lib/utils";
 
 function DesignerElementButton({ formElement }: { formElement: FormElement }) {
   const draggable = useDraggable({
@@ -17,7 +18,10 @@ function DesignerElementButton({ formElement }: { formElement: FormElement }) {
     <Button
       ref={draggable.setNodeRef}
       variant={"outline"}
-      className="flex flex-col gap-2 h-[120px] w-[120px] cursor-grab"
+      className={cn(
+        "flex flex-col gap-2 h-[120px] w-[120px] cursor-grab",
+        draggable.isDragging && "ring-2 ring-primary"
+      )}
       {...draggable.listeners}
       {...draggable.attributes}
     >
