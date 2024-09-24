6ca4de8b8ff713038d209b7f85cd7d4fbdcf5083
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-25 03:39:46 +0800

    feat: show DesignerElementProperties if has selectedElement otherwise show DesignerElements

--- components/builder/form-builder/designer/sidebar/DesignerSidebar.tsx
+++ components/builder/form-builder/designer/sidebar/DesignerSidebar.tsx
@@ -1,9 +1,14 @@
+import useDesigner from "@/hooks/use-designer";
 import DesignerElements from "@/components/builder/form-builder/designer/sidebar/DesignerElements";
+import DesignerElementProperties from "@/components/builder/form-builder/designer/sidebar/DesignerElementProperties";
 
 function DesignerSidebar() {
+  const { selectedElement } = useDesigner();
+
   return (
     <aside className="w-[400px] max-w-[400px] flex flex-col flex-grow gap-2 border-l-2 border-muted p-4 bg-background overflow-y-auto h-full">
-      <DesignerElements />
+      {!selectedElement && <DesignerElements />}
+      {selectedElement && <DesignerElementProperties />}
     </aside>
   );
 }
