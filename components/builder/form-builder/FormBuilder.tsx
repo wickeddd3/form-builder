cdcce0c95672b9ea0e3a5073160b809f74393513
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-24 23:04:21 +0800

    feat: wrap FormBuilder in DesignerContextProvider

--- components/builder/form-builder/FormBuilder.tsx
+++ components/builder/form-builder/FormBuilder.tsx
@@ -3,13 +3,16 @@
 import { Form } from "@prisma/client";
 import ActionButtons from "@/components/builder/form-builder/action-buttons/ActionButtons";
 import Designer from "@/components/builder/form-builder/designer/Designer";
+import DesignerContextProvider from "@/context/DesignerContext";
 
 function FormBuilder({ form }: { form: Form }) {
   return (
-    <main className="flex flex-col w-full">
-      <ActionButtons form={form} />
-      <Designer />
-    </main>
+    <DesignerContextProvider>
+      <main className="flex flex-col w-full">
+        <ActionButtons form={form} />
+        <Designer />
+      </main>
+    </DesignerContextProvider>
   );
 }
 
