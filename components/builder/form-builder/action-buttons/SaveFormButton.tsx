401c86066c71aff1088d368f6f7d0146971664de
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-26 03:55:58 +0800

    feat: update form and save form elements

--- components/builder/form-builder/action-buttons/SaveFormButton.tsx
+++ components/builder/form-builder/action-buttons/SaveFormButton.tsx
@@ -1,11 +1,44 @@
 import { Button } from "@/components/ui/button";
 import { MdOutlineSave } from "react-icons/md";
+import useDesigner from "@/hooks/use-designer";
+import { updateForm } from "@/actions/form";
+import { toast } from "@/hooks/use-toast";
+import { useTransition } from "react";
+import { FaSpinner } from "react-icons/fa";
 
-function SaveFormButton() {
+function SaveFormButton({ id }: { id: string }) {
+  const { elements } = useDesigner();
+  const [loading, startTransition] = useTransition();
+
+  const updateFormContent = async () => {
+    try {
+      const jsonElements = JSON.stringify(elements);
+      await updateForm(id, jsonElements);
+      toast({
+        title: "Success",
+        description: "Your form has been saved",
+      });
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Something went wrong",
+        variant: "destructive",
+      });
+    }
+  };
+
   return (
-    <Button variant={"outline"} className="gap-2">
+    <Button
+      variant={"outline"}
+      className="gap-2"
+      disabled={loading}
+      onClick={() => {
+        startTransition(updateFormContent);
+      }}
+    >
       <MdOutlineSave className="h-5 w-5" />
       Save
+      {loading && <FaSpinner className="animate-spin" />}
     </Button>
   );
 }
