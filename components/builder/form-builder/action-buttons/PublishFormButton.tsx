4c25bd8e436014af5c5b2a929c81e8b57f1e7c4d
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-26 04:58:20 +0800

    feat: add alert dialog and publishForm on dialog confirm

--- components/builder/form-builder/action-buttons/PublishFormButton.tsx
+++ components/builder/form-builder/action-buttons/PublishFormButton.tsx
@@ -1,15 +1,75 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
+import { AlertDialogTrigger } from "@radix-ui/react-alert-dialog";
+import { FaSpinner } from "react-icons/fa";
 import { MdOutlinePublish } from "react-icons/md";
+import { useTransition } from "react";
+import { toast } from "@/hooks/use-toast";
+import { publishForm } from "@/actions/form";
 
-function PublishFormButton() {
+function PublishFormButton({ id }: { id: string }) {
+  const [loading, startTransition] = useTransition();
+
+  async function publishFormContent() {
+    try {
+      await publishForm(id);
+      toast({
+        title: "Success",
+        description: "Your form is now available to the public",
+      });
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Something went wrong",
+      });
+    }
+  }
+
   return (
-    <Button
-      variant={"outline"}
-      className="gap-2 bg-gradient-to-r from-cyan-500 to-blue-600 text-white hover:text-white"
-    >
-      <MdOutlinePublish className="h-5 w-5" />
-      Publish
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button
+          variant={"outline"}
+          className="gap-2 bg-gradient-to-r from-cyan-500 to-blue-600 text-white hover:text-white"
+        >
+          <MdOutlinePublish className="h-5 w-5" />
+          Publish
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle className="text-center font-bold">
+            Are you sure?
+          </AlertDialogTitle>
+          <AlertDialogDescription className="text-center text-pretty text-md py-6">
+            This action cannot be undone. After publishing you will not be able
+            to edit this form. By publishing this form you will make it
+            available to the public and you will be able to collect submissions.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            disabled={loading}
+            onClick={(e) => {
+              e.preventDefault();
+              startTransition(publishFormContent);
+            }}
+          >
+            Proceed {loading && <FaSpinner className="animate-spin" />}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
 
