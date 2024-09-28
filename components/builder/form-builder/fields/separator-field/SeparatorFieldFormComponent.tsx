d51890cfb446c2ee3c0dc5665c526b5bfc0c30ea
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-29 00:07:07 +0800

    feat: add SeparatorFieldFormComponent

---
+++ components/builder/form-builder/fields/separator-field/SeparatorFieldFormComponent.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { Separator } from "@/components/ui/separator";
+
+function SeparatorFieldFormComponent() {
+  return <Separator />;
+}
+
+export default SeparatorFieldFormComponent;
