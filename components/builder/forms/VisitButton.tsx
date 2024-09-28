f92fbcfdcf57b65dc04a6c5fa5d00f5657207e3b
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-28 15:25:50 +0800

    feat: add VisitButton

---
+++ components/builder/forms/VisitButton.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+
+function VisitButton({ shareUrl }: { shareUrl: string }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null; // avoiding window not defined error
+  }
+
+  const shareLink = `${window.location.origin}/submit/${shareUrl}`;
+
+  return (
+    <Button
+      className="w-[200px]"
+      onClick={() => {
+        window.open(shareLink, "_blank");
+      }}
+    >
+      Visit
+    </Button>
+  );
+}
+
+export default VisitButton;
