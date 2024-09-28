461496b38c4a4f5da5644fabb24d0a45547ad732
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-29 02:12:26 +0800

    feat: add NumberField schema

---
+++ components/builder/form-builder/fields/number-field/schema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const propertiesSchema = z.object({
+  label: z.string().min(2).max(50),
+  helperText: z.string().max(200),
+  required: z.boolean().default(false),
+  placeholder: z.string().max(50),
+});
+
+export type propertiesSchemaType = z.infer<typeof propertiesSchema>;
