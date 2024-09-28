c7ebc55779c8001f884739f000a64d34f516aa94
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-29 02:11:49 +0800

    feat: add NumberFieldFormComponent

---
+++ components/builder/form-builder/fields/number-field/NumberFieldFormComponent.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import {
+  FormElementInstance,
+  SubmitFunction,
+} from "@/components/builder/form-builder/FormElements";
+import { CustomInstance, NumberFieldFormElement } from "./NumberField";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+
+function NumberFieldFormComponent({
+  elementInstance,
+  submitValue,
+  isInvalid,
+  defaultValue,
+}: {
+  elementInstance: FormElementInstance;
+  submitValue?: SubmitFunction;
+  isInvalid?: boolean;
+  defaultValue?: string;
+}) {
+  const element = elementInstance as CustomInstance;
+  const { label, required, placeholder, helperText } = element.extraAttributes;
+  const [value, setValue] = useState(defaultValue || "");
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setError(isInvalid === true);
+  }, [isInvalid]);
+
+  return (
+    <div className="flex flex-col gap-2 w-full">
+      <Label className={cn(error && "text-red-500")}>
+        {label}
+        {required && "*"}
+      </Label>
+      <Input
+        type="number"
+        className={cn(error && "border-red-500")}
+        placeholder={placeholder}
+        onChange={(e) => setValue(e.target.value)}
+        onBlur={(e) => {
+          if (!submitValue) return;
+          const valid = NumberFieldFormElement.validate(
+            element,
+            e.target.value
+          );
+          setError(!valid);
+          if (!valid) return;
+          submitValue(element.id, e.target.value);
+        }}
+        value={value}
+      />
+      {helperText && (
+        <p
+          className={cn(
+            "text-muted-foreground text-[0.8rem]",
+            error && "text-red-500"
+          )}
+        >
+          {helperText}
+        </p>
+      )}
+    </div>
+  );
+}
+
+export default NumberFieldFormComponent;
