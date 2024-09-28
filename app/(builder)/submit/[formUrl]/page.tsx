f73afe63e59ccf88d2c56edbed233d29f6729c29
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-28 15:23:30 +0800

    feat: add submit Page

---
+++ app/(builder)/submit/[formUrl]/page.tsx
@@ -0,0 +1,17 @@
+import { getFormContentByUrl } from "@/actions/form";
+import { FormElementInstance } from "@/components/builder/form-builder/FormElements";
+import FormSubmitComponent from "@/components/builder/forms/FormSubmitComponent";
+
+async function Page({ params }: { params: { formUrl: string } }) {
+  const form = await getFormContentByUrl(params.formUrl);
+
+  if (!form) {
+    throw new Error("Form not found");
+  }
+
+  const formContent = JSON.parse(form.content) as FormElementInstance[];
+
+  return <FormSubmitComponent formUrl={params.formUrl} content={formContent} />;
+}
+
+export default Page;
