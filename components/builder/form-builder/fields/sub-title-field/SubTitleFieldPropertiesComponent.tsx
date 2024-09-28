4d3ee70735b5ef725771145ac9b3ea5cb6f5e331
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-28 23:40:40 +0800

    feat: add SubTitleFieldPropertiesComponent

---
+++ components/builder/form-builder/fields/sub-title-field/SubTitleFieldPropertiesComponent.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { FormElementInstance } from "@/components/builder/form-builder/FormElements";
+import { CustomInstance } from "./SubTitleField";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { useEffect } from "react";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import useDesigner from "@/hooks/use-designer";
+import { propertiesSchema, propertiesSchemaType } from "./schema";
+
+function SubTitleFieldPropertiesComponent({
+  elementInstance,
+}: {
+  elementInstance: FormElementInstance;
+}) {
+  const element = elementInstance as CustomInstance;
+  const { updateElement } = useDesigner();
+  const form = useForm<propertiesSchemaType>({
+    resolver: zodResolver(propertiesSchema),
+    mode: "onBlur",
+    defaultValues: {
+      title: element.extraAttributes.title,
+    },
+  });
+
+  useEffect(() => {
+    form.reset(element.extraAttributes);
+  }, [element, form]);
+
+  function applyChanges(values: propertiesSchemaType) {
+    const { title } = values;
+    updateElement(element.id, {
+      ...element,
+      extraAttributes: {
+        title,
+      },
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onBlur={form.handleSubmit(applyChanges)}
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className="space-y-3"
+      >
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") e.currentTarget.blur();
+                  }}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+}
+
+export default SubTitleFieldPropertiesComponent;
