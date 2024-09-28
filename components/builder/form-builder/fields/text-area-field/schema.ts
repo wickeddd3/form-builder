48a8ad135eae70691264f906b3023839466c4af0
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-29 02:30:06 +0800

    feat: add TextAreaField schema

---
+++ components/builder/form-builder/fields/text-area-field/schema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const propertiesSchema = z.object({
+  label: z.string().min(2).max(50),
+  helperText: z.string().max(200),
+  required: z.boolean().default(false),
+  placeholder: z.string().max(50),
+  rows: z.number().min(1).max(10),
+});
+
+export type propertiesSchemaType = z.infer<typeof propertiesSchema>;
