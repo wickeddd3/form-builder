29fec2eb14d8d29b1cdbe0c38e9cc873b82f8abd
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-28 15:28:04 +0800

    feat: add TextFieldFormComponent validation

--- components/builder/form-builder/fields/text-field/TextFieldFormComponent.tsx
+++ components/builder/form-builder/fields/text-field/TextFieldFormComponent.tsx
@@ -1,27 +1,61 @@
 "use client";
 
-import { FormElementInstance } from "@/components/builder/form-builder/FormElements";
-import { CustomInstance } from "./TextField";
+import {
+  FormElementInstance,
+  SubmitFunction,
+} from "@/components/builder/form-builder/FormElements";
+import { CustomInstance, TextFieldFormElement } from "./TextField";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
 
 function TextFieldFormComponent({
   elementInstance,
+  submitValue,
+  isInvalid,
 }: {
   elementInstance: FormElementInstance;
+  submitValue?: SubmitFunction;
+  isInvalid?: boolean;
 }) {
   const element = elementInstance as CustomInstance;
   const { label, required, placeholder, helperText } = element.extraAttributes;
+  const [value, setValue] = useState("");
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setError(isInvalid === true);
+  }, [isInvalid]);
 
   return (
     <div className="flex flex-col gap-2 w-full">
-      <Label>
+      <Label className={cn(error && "text-red-500")}>
         {label}
         {required && "*"}
       </Label>
-      <Input placeholder={placeholder} />
+      <Input
+        className={cn(error && "border-red-500")}
+        placeholder={placeholder}
+        onChange={(e) => setValue(e.target.value)}
+        onBlur={(e) => {
+          if (!submitValue) return;
+          const valid = TextFieldFormElement.validate(element, e.target.value);
+          setError(!valid);
+          if (!valid) return;
+          submitValue(element.id, e.target.value);
+        }}
+        value={value}
+      />
       {helperText && (
-        <p className="text-muted-foreground text-[0.8rem]">{helperText}</p>
+        <p
+          className={cn(
+            "text-muted-foreground text-[0.8rem]",
+            error && "text-red-500"
+          )}
+        >
+          {helperText}
+        </p>
       )}
     </div>
   );
