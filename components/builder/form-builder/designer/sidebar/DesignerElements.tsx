4b413dd4f4dacc351f10dc1f8cd89c925715c4b1
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-28 20:12:59 +0800

    feat: add TitleField in DesignerElements

--- components/builder/form-builder/designer/sidebar/DesignerElements.tsx
+++ components/builder/form-builder/designer/sidebar/DesignerElements.tsx
@@ -5,7 +5,10 @@ function DesignerElements() {
   return (
     <div className="flex flex-col gap-4">
       <h6 className="text-md font-medium">Form Elements</h6>
-      <DesignerElementButton formElement={FormElements.TextField} />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 place-items-center">
+        <DesignerElementButton formElement={FormElements.TextField} />
+        <DesignerElementButton formElement={FormElements.TitleField} />
+      </div>
     </div>
   );
 }
