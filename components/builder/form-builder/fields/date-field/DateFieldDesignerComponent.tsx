5b34199631af4a9ca1d220e39a4ea2c0ef921186
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-29 02:57:57 +0800

    feat: add DateFieldDesignerComponent

---
+++ components/builder/form-builder/fields/date-field/DateFieldDesignerComponent.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { FormElementInstance } from "@/components/builder/form-builder/FormElements";
+import { CustomInstance } from "./DateField";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { CalendarIcon } from "lucide-react";
+
+function DateFieldDesignerComponent({
+  elementInstance,
+}: {
+  elementInstance: FormElementInstance;
+}) {
+  const element = elementInstance as CustomInstance;
+  const { label, required, helperText } = element.extraAttributes;
+
+  return (
+    <div className="flex flex-col gap-2 w-full">
+      <Label>
+        {label}
+        {required && "*"}
+      </Label>
+      <Button
+        variant={"outline"}
+        className="w-full justify-start text-left font-normal"
+      >
+        <CalendarIcon className="mr-2 h-4 w-4" />
+        <span>Pick a date</span>
+      </Button>
+      {helperText && (
+        <p className="text-muted-foreground text-[0.8rem]">{helperText}</p>
+      )}
+    </div>
+  );
+}
+
+export default DateFieldDesignerComponent;
