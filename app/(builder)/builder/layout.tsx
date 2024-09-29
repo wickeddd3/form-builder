17c46176cafd4b8d9dcff773c6d85c06212821ee
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-29 18:09:18 +0800

    add: builder layout h-screen style

--- app/(builder)/builder/layout.tsx
+++ app/(builder)/builder/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
+    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen h-screen">
       <Navbar />
       <main className="flex w-full flex-grow">{children}</main>
     </div>
