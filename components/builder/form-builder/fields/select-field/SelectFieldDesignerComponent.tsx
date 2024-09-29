486e21182b411bd286c775f240781f0aef0469b6
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-29 13:52:05 +0800

    feat: add SelectFieldDesignerComponent

---
+++ components/builder/form-builder/fields/select-field/SelectFieldDesignerComponent.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { FormElementInstance } from "@/components/builder/form-builder/FormElements";
+import { CustomInstance } from "./SelectField";
+import { Select, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
+
+function SelectFieldDesignerComponent({
+  elementInstance,
+}: {
+  elementInstance: FormElementInstance;
+}) {
+  const element = elementInstance as CustomInstance;
+  const { label, required, placeholder, helperText } = element.extraAttributes;
+
+  return (
+    <div className="flex flex-col gap-2 w-full">
+      <Label>
+        {label}
+        {required && "*"}
+      </Label>
+      <Select>
+        <SelectTrigger className="w-full">
+          <SelectValue placeholder={placeholder} />
+        </SelectTrigger>
+      </Select>
+      {helperText && (
+        <p className="text-muted-foreground text-[0.8rem]">{helperText}</p>
+      )}
+    </div>
+  );
+}
+
+export default SelectFieldDesignerComponent;
