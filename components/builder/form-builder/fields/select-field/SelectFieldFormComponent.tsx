8bcc6b739404af1932d19ab662ad489080efbe7f
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-29 13:52:14 +0800

    feat: add SelectFieldFormComponent

---
+++ components/builder/form-builder/fields/select-field/SelectFieldFormComponent.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import {
+  FormElementInstance,
+  SubmitFunction,
+} from "@/components/builder/form-builder/FormElements";
+import { CustomInstance, SelectFieldFormElement } from "./SelectField";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+
+function SelectFieldFormComponent({
+  elementInstance,
+  submitValue,
+  isInvalid,
+  defaultValue,
+}: {
+  elementInstance: FormElementInstance;
+  submitValue?: SubmitFunction;
+  isInvalid?: boolean;
+  defaultValue?: string;
+}) {
+  const element = elementInstance as CustomInstance;
+  const { label, required, placeholder, helperText, options } =
+    element.extraAttributes;
+  const [value, setValue] = useState(defaultValue || "");
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setError(isInvalid === true);
+  }, [isInvalid]);
+
+  return (
+    <div className="flex flex-col gap-2 w-full">
+      <Label className={cn(error && "text-red-500")}>
+        {label}
+        {required && "*"}
+      </Label>
+      <Select
+        defaultValue={value}
+        onValueChange={(value) => {
+          setValue(value);
+          if (!submitValue) return;
+          const valid = SelectFieldFormElement.validate(element, value);
+          setError(!valid);
+          submitValue(element.id, value);
+        }}
+      >
+        <SelectTrigger className={cn("w-full", error && "border-red-500")}>
+          <SelectValue placeholder={placeholder} />
+        </SelectTrigger>
+        <SelectContent>
+          {options.map((option) => (
+            <SelectItem key={option} value={option}>
+              {option}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      {helperText && (
+        <p
+          className={cn(
+            "text-muted-foreground text-[0.8rem]",
+            error && "text-red-500"
+          )}
+        >
+          {helperText}
+        </p>
+      )}
+    </div>
+  );
+}
+
+export default SelectFieldFormComponent;
