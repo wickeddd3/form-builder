db4b5ba2d49bd778d342d0e6927ac20dfbbe99ee
wickeddd3 <pablophilipandrew@gmail.com>
2024-10-02 00:00:03 +0800

    cut: remove forms route

--- app/(builder)/forms/[id]/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react";
-
-function Layout({ children }: { children: ReactNode }) {
-  return <div className="flex w-full flex-col flex-grow mx-auto">{children}</div>;
-}
-
-export default Layout;
