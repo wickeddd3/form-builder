68c7ad0e7a449cf971861b20faa0eff7d0646482
wickeddd3 <pablophilipandrew@gmail.com>
2024-10-02 00:23:23 +0800

    add: redirect to builder page if / route is visited and remove /forms  in protectedRoutes

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,14 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/builder(.*)",
-  "/forms(.*)",
-  "/submit(.*)",
-]);
+const isProtectedRoute = createRouteMatcher(["/builder(.*)", "/submit(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
+  const { pathname } = req.nextUrl;
   if (isProtectedRoute(req)) auth().protect();
+  if (pathname === "/") {
+    return NextResponse.redirect(new URL(`/builder`, req.nextUrl));
+  }
 });
 
 export const config = {
