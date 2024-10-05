b8c6c858ffaa3913b1ea7de014ab910e3c315fc4
wickeddd3 <pablophilipandrew@gmail.com>
2024-10-05 20:22:43 +0800

    add: full height

--- components/builder/form-builder/fields/text-area-field/TextAreaFieldDesignerComponent.tsx
+++ components/builder/form-builder/fields/text-area-field/TextAreaFieldDesignerComponent.tsx
@@ -14,7 +14,7 @@ function TextAreaFieldDesignerComponent({
   const { label, required, placeholder, helperText } = element.extraAttributes;
 
   return (
-    <div className="flex flex-col gap-2 w-full">
+    <div className="flex flex-col gap-2 w-full h-full">
       <Label>
         {label}
         {required && "*"}
