0c6788af6a695b56f74ffb397a0e7425389fae52
wickeddd3 <pablophilipandrew@gmail.com>
2024-10-16 01:19:18 +0800

    refactor: FormLinkShare

--- components/builder/forms/FormLinkShare.tsx
+++ components/builder/forms/FormLinkShare.tsx
@@ -6,7 +6,11 @@ import { toast } from "@/hooks/use-toast";
 import { useEffect, useState } from "react";
 import { ImShare } from "react-icons/im";
 
-function FormLinkShare({ shareUrl }: { shareUrl: string }) {
+interface FormLinkShareProps {
+  shareUrl: string;
+}
+
+function FormLinkShare({ shareUrl }: FormLinkShareProps) {
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
@@ -19,19 +23,18 @@ function FormLinkShare({ shareUrl }: { shareUrl: string }) {
 
   const shareLink = `${window.location.origin}/submit/${shareUrl}`;
 
+  const handleCopyLink = () => {
+    navigator.clipboard.writeText(shareLink);
+    toast({
+      title: "Copied!",
+      description: "Link copied to clipboard",
+    });
+  };
+
   return (
     <div className="flex flex-grow gap-4 items-center">
       <Input value={shareLink} readOnly />
-      <Button
-        className="max-w-[250px]"
-        onClick={() => {
-          navigator.clipboard.writeText(shareLink);
-          toast({
-            title: "Copied!",
-            description: "Link copied to clipboard",
-          });
-        }}
-      >
+      <Button className="max-w-[250px]" onClick={handleCopyLink}>
         <ImShare className="mr-2 h-4 w-4" />
         Share link
       </Button>
