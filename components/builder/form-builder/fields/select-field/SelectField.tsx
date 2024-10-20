f51ff75c9cdc83cc186d2da534ffd17cb1b0f062
wickeddd3 <pablophilipandrew@gmail.com>
2024-10-20 23:31:01 +0800

    refactor: import extraAttributes and validate

--- components/builder/form-builder/fields/select-field/SelectField.tsx
+++ components/builder/form-builder/fields/select-field/SelectField.tsx
@@ -3,22 +3,15 @@
 import {
   ElementsType,
   FormElement,
-  FormElementInstance,
 } from "@/components/builder/form-builder/FormElements";
 import { RxDropdownMenu } from "react-icons/rx";
 import SelectFieldDesignerComponent from "./SelectFieldDesignerComponent";
 import SelectFieldPropertiesComponent from "./SelectFieldPropertiesComponent";
 import SelectFieldFormComponent from "./SelectFieldFormComponent";
+import { extraAttributes } from "./attributes";
+import { validate } from "./utils";
 
 const type: ElementsType = "SelectField";
-
-const extraAttributes = {
-  label: "Select field",
-  helperText: "Helper text",
-  required: false,
-  placeholder: "Value here...",
-  options: [],
-};
 
 export const SelectFieldFormElement: FormElement = {
   type,
@@ -34,20 +27,5 @@ export const SelectFieldFormElement: FormElement = {
   designerComponent: SelectFieldDesignerComponent,
   propertiesComponent: SelectFieldPropertiesComponent,
   formComponent: SelectFieldFormComponent,
-
-  validate: (
-    formElement: FormElementInstance,
-    currentValue: string
-  ): boolean => {
-    const element = formElement as CustomInstance;
-    if (element.extraAttributes.required) {
-      return currentValue.length > 0;
-    }
-
-    return true;
-  },
-};
-
-export type CustomInstance = FormElementInstance & {
-  extraAttributes: typeof extraAttributes;
+  validate,
 };
