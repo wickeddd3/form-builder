23ff31cc8d98461436378746bf7903e297895e6f
wickeddd3 <pablophilipandrew@gmail.com>
2024-10-21 03:27:58 +0800

    refactor: import extraAttributes and validate

--- components/builder/form-builder/fields/number-field/NumberField.tsx
+++ components/builder/form-builder/fields/number-field/NumberField.tsx
@@ -3,21 +3,15 @@
 import {
   ElementsType,
   FormElement,
-  FormElementInstance,
 } from "@/components/builder/form-builder/FormElements";
 import { Bs123 } from "react-icons/bs";
 import NumberFieldDesignerComponent from "./NumberFieldDesignerComponent";
 import NumberFieldPropertiesComponent from "./NumberFieldPropertiesComponent";
 import NumberFieldFormComponent from "./NumberFieldFormComponent";
+import { extraAttributes } from "./attributes";
+import { validate } from "./utils";
 
 const type: ElementsType = "NumberField";
-
-const extraAttributes = {
-  label: "Number field",
-  helperText: "Helper text",
-  required: false,
-  placeholder: "0",
-};
 
 export const NumberFieldFormElement: FormElement = {
   type,
@@ -33,20 +27,5 @@ export const NumberFieldFormElement: FormElement = {
   designerComponent: NumberFieldDesignerComponent,
   propertiesComponent: NumberFieldPropertiesComponent,
   formComponent: NumberFieldFormComponent,
-
-  validate: (
-    formElement: FormElementInstance,
-    currentValue: string
-  ): boolean => {
-    const element = formElement as CustomInstance;
-    if (element.extraAttributes.required) {
-      return currentValue.length > 0;
-    }
-
-    return true;
-  },
-};
-
-export type CustomInstance = FormElementInstance & {
-  extraAttributes: typeof extraAttributes;
+  validate,
 };
