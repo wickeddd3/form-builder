ac0be4e418f7bf040be62fa1fe94586ad4a91d78
wickeddd3 <pablophilipandrew@gmail.com>
2024-10-21 22:31:41 +0800

    refactor: import extraAttributes and validate

--- components/builder/form-builder/fields/checkbox-field/CheckboxField.tsx
+++ components/builder/form-builder/fields/checkbox-field/CheckboxField.tsx
@@ -3,20 +3,15 @@
 import {
   ElementsType,
   FormElement,
-  FormElementInstance,
 } from "@/components/builder/form-builder/FormElements";
 import { IoMdCheckbox } from "react-icons/io";
 import CheckboxFieldDesignerComponent from "./CheckboxFieldDesignerComponent";
 import CheckboxFieldPropertiesComponent from "./CheckboxFieldPropertiesComponent";
 import CheckboxFieldFormComponent from "./CheckboxFieldFormComponent";
+import { extraAttributes } from "./attributes";
+import { validate } from "./utils";
 
 const type: ElementsType = "CheckboxField";
-
-const extraAttributes = {
-  label: "Checkbox field",
-  helperText: "Helper text",
-  required: false,
-};
 
 export const CheckboxFieldFormElement: FormElement = {
   type,
@@ -32,20 +27,5 @@ export const CheckboxFieldFormElement: FormElement = {
   designerComponent: CheckboxFieldDesignerComponent,
   propertiesComponent: CheckboxFieldPropertiesComponent,
   formComponent: CheckboxFieldFormComponent,
-
-  validate: (
-    formElement: FormElementInstance,
-    currentValue: string
-  ): boolean => {
-    const element = formElement as CustomInstance;
-    if (element.extraAttributes.required) {
-      return currentValue === "true";
-    }
-
-    return true;
-  },
-};
-
-export type CustomInstance = FormElementInstance & {
-  extraAttributes: typeof extraAttributes;
+  validate,
 };
