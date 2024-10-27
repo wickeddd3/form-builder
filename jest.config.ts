7803849c7da075b8b833c35366fece15f291e221
wickeddd3 <pablophilipandrew@gmail.com>
2024-10-28 02:01:46 +0800

    chore: add jest testMatch for unit test

--- jest.config.ts
+++ jest.config.ts
@@ -169,10 +169,11 @@ const config: Config = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: [
+    // "**/__tests__/**/*.[jt]s?(x)",
+    // "**/?(*.)+(spec|test).[tj]s?(x)"
+    "**/tests/unit/**/*.[jt]s?(x)",
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: ["/node_modules/"],
